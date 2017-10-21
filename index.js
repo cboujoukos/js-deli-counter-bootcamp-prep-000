8d7c5abffd129cfc78e2cb38d04a475acb199e2d
Christopher Michael Boujoukos <cboujoukos@gmail.com>
2017-10-21 15:22:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ function takeANumber(katzDeli, name) {
   katzDeli.push(name)
   return (`Welcome, ${name}. You are number ` + katzDeli.length + ' in line.')
 }
+function nowServing() {
+  if (katzDeli.length > 1) {
+    return ('Currently serving ' + katzDeli[0]);
+    katzDeli.shift();
+  } else {
+    return ("There is nobody waiting to be served!")
+  }
+}
